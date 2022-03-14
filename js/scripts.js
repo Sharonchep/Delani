fe6e6cbd80186868bfc6f6a7d96f95740afd8ce2
Sharonchep <sharon30@gmail.com>
2022-03-14 10:35:51 +0300

    add new changes

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $(".click").click(function() {
+    $(".clicked").show();
+    $(".click").hide();
+    $("#clickable").click(function() {
+    $(".click").show();
+    $(".clicked").hide();
+});
+});
+});
+// $(document).ready(function() {
+//   $(".bad").click(function() {
+//     $(".worst").show();
+//     $(".bad").hide();
+//     $("#worse").click(function() {
+//     $(".bad").show();
+//     $(".worst").hide();
+// });
+// });
+// });
+// $(document).ready(function() {
+//   $(".good").click(function() {
+//     $(".best").show();
+//     $(".good").hide();
+//     $("#better").click(function() {
+//     $(".good").show();
+//     $(".best").hide();
+// });
+// });
+// });
+// $(document).ready(function(){
+// $(".image").hover(function(){
+//   $(".text-block").toggle();
+//   })
+// });
