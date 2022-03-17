33583a2b893b190cb5320048155da221c1dcd7ff
Sharonchep <sharon30@gmail.com>
2022-03-17 09:41:22 +0300

    add new changes

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,48 @@ $(document).ready(function() {
     $(".clicked").hide();
   });
 });
+$(".good").click(function() {
+  $(".best").show();
+  $(".good").hide();
+  $("#better").click(function() {
+  $(".good").show();
+  $(".best").hide();
 });
-$(document).ready(function() {
-  $(".bad").click(function() {
-    $(".worst").show();
-    $(".bad").hide();
-    $("#worse").click(function() {
-    $(".bad").show();
-    $(".worst").hide();
+});
+$(".bad").click(function() {
+  $(".worst").show();
+  $(".bad").hide();
+  $("#worse").click(function() {
+  $(".bad").show();
+  $(".worst").hide();
+});
+});
+$("#one").hover(function(){
+  $(".text-block1").toggle();
   });
-});
-});
-$(document).ready(function() {
-  $(".good").click(function() {
-    $(".best").show();
-    $(".good").hide();
-    $("#better").click(function() {
-    $(".good").show();
-    $(".best").hide();
+$("#two").hover(function(){
+  $(".text-block2").toggle();
   });
+$("#three").hover(function(){
+  $(".text-block3").toggle();
+  });
+$("#four").hover(function(){
+  $(".text-block4").toggle();
+  });
+$("#five").hover(function(){
+  $(".text-block5").toggle();
+  });
+$("#six").hover(function(){
+  $(".text-block6").toggle();
+  });
+$("#seven").hover(function(){
+  $(".text-block7").toggle();
+  });
+$("#eight").hover(function(){
+  $(".text-block8").toggle();
+  });
+  
 });
-});
-$(document).ready(function(){
-  $(".image").hover(function(){
-    $(".text-block").toggle();
-  })
-});
+
+
+
